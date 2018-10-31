3e149cce2a91f13984b394f4c9cbe5376c2ca7b7
lyn <lijiayin0212@gmail.com>
2018-10-31 13:17:43 +0100

    Bon je viens de remarquer que le test n'a pas pu être push, je reverrai bientôt

---
+++ src/test/GomTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+import piece.PieceGom;
+import piece.Couleur;
+import piece.Forme;
+
+public class GomTest {
+
+	@Test 
+	public void couleurTest(){
+		Couleur c = Couleur.BLEU ;
+		System.out.println(c.toString());
+		
+	}
+
+}
